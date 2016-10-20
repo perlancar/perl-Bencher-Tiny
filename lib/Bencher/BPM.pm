ed73f0dee6209df13bf54da1f7a7e30b095bb979
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-10-20 11:13:18 +0700

    Initial commit

---
+++ lib/Bencher/BPM.pm
@@ -0,0 +1,19 @@
+package Bencher::BPM;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Simple script to run benchmark scenario with Benchmark.pm
+
+=head1 DESCRIPTION
+
+This distribution includes the script L<bencher-bpm> to run benchmark scenario
+with L<Benchmark> (Benchmark.pm). It lacks L<bencher> features like filtering
+participants/datasets/modules, listing participants/datasets or other actions.
+But it is small and does not have any non-core dependency.
+
+
+=head1 SEE ALSO
+
+L<bencher> from L<Bencher>
